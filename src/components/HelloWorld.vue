fa473144a739c1603b43f3789356c1648d7c2c47
Lm <lmy071@icloud.com>
2025-05-30 11:29:19 +0800

    ai - 新增ts&vue版本升级

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -4,12 +4,12 @@
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      <a href="https://vitejs.dev" target="_blank" rel="noopener">Vite Documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Installed Plugins</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://github.com/vitejs/vite-plugin-vue" target="_blank" rel="noopener">@vitejs/plugin-vue</a></li>
+      <li><a href="https://www.typescriptlang.org/" target="_blank" rel="noopener">TypeScript</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -22,21 +22,25 @@
     <h3>Ecosystem</h3>
     <ul>
       <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
+      <li><a href="https://pinia.vuejs.org" target="_blank" rel="noopener">pinia</a></li>
+      <li><a href="https://github.com/vuejs/devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
     </ul>
   </div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: {
+      type: String,
+      required: true
+    }
   }
-}
+})
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
